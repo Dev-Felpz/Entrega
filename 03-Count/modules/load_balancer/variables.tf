32f8fc06cdaca107d8ab79569938df5ae6e4c937
Dev-Felpz <168293882+Dev-Felpz@users.noreply.github.com>
2025-05-17 10:45:10 -0300

    Delete 03-Count directory

--- 03-Count/modules/load_balancer/variables.tf
+++
@@ -1,6 +0,0 @@
-# modules/load_balancer/variables.tf
-variable "quantidade_nos" {
-  description = "Quantidade de nós no Load Balancer"
-  type        = number
-  default     = 3
-}
--- 03-Count/state.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "base-config-359066"
-    key    = "workspaces"
-    region = "us-east-1"
-  }
-}
